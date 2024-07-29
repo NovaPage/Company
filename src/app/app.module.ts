45b97fa76deae44373b83451d61a09ce89da1c42
NovaPage <novapagecompany@gmail.com>
2024-07-29 10:57:26 -0500

    Version de prueba

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainComponent } from './main/main.component';
+import { NavbarmainComponent } from './navbarmain/navbarmain.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    NavbarmainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/navbarmain/navbarmain.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbarmain',
+  templateUrl: './navbarmain.component.html',
+  styleUrls: ['./navbarmain.component.scss']
+})
+export class NavbarmainComponent {
+
+}
